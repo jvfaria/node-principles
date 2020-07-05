740b2f881bfac395516b908b6a51b14a3bf40f52
JVitor22 <jvfaria015@gmail.com>
2020-07-05 15:17:42 -0300

    completed

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface TransactionRequest {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,35 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: TransactionRequest): Transaction {
+    const types = ['income', 'outcome'];
+    const filteredType = types.find(t => t === type); // Filter the types ['income' or 'outcome']
+    if (!filteredType) {
+      // Verify if this type is one of the two types valid
+      throw Error('Error creating transaction, review your values!'); // Throw an error if it's not
+    } else if (filteredType === 'outcome') {
+      // If the transaction is 'outcome', it means that we need to have a VALID balance to do it (incomes x outcomes)
+      const balance = this.transactionsRepository.getBalance();
+      if (balance.total - value < 0) {
+        // Verify if the total value of the balance (incomes - outcomes) is valid
+        throw Error('Your balance is not valid! Cannot do this transaction!');
+      } else {
+        // Valid balance =>> creates a new transaction
+        const transaction = this.transactionsRepository.create({
+          title,
+          value,
+          type,
+        });
+        return transaction;
+      }
+    } else {
+      const transaction = this.transactionsRepository.create({
+        title,
+        value,
+        type,
+      });
+      return transaction;
+    }
   }
 }
 
